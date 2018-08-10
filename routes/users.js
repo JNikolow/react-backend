0232b9e581f1c663c500aa874a02545540756f9c
Joro <joro.nikolow@gmail.com>
2018-08-10 16:23:16 +0300

    Add users post

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,9 @@
 var express = require('express');
+var Validator = require('validator');
+var bcrypt = require('bcrypt');
+var isEmpty = require('lodash/isEmpty');
+
+
 var router = express.Router();
 
 /* GET users listing. */
@@ -14,6 +19,48 @@ router.get('/', function(req, res, next) {
   	id: 2,
   	username: "D0loresH4ze"
   }]);
+});
+
+function validateInput(data) {
+    let errors = {};
+
+    if (Validator.isEmpty(data.username)) {
+      errors.username = 'This field is required';
+    }
+    if (Validator.isEmpty(data.email)) {
+      errors.email = 'This field is required';
+    }
+    if (!Validator.isEmail(data.email)) {
+      errors.email = 'Email is invalid';
+    }
+    if (Validator.isEmpty(data.password)) {
+      errors.password = 'This field is required';
+    }
+    if (Validator.isEmpty(data.passwordConfirmation)) {
+      errors.passwordConfirmation = 'This field is required';
+    }
+    if (!Validator.equals(data.password, data.passwordConfirmation)) {
+      errors.passwordConfirmation = 'Passwords must match';
+    }
+    if (Validator.isEmpty(data.timezone)) {
+      errors.timezone = 'This field is required';
+    }
+      
+    console.log('isValid : ', isEmpty(errors));
+    console.log(errors);
+
+    return {
+        errors,
+        isValid: isEmpty(errors)
+    }
+}
+
+router.post('/', (req, res) => {
+  const { errors, isValid } = validateInput(req.body);
+
+  if (!isValid) {
+      res.status(400).json(errors);
+  }
 });
 
 module.exports = router;
